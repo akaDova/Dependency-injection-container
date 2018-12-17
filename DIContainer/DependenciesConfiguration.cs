98a72bd9ef41daf96ee3dcabe7b7033783d7dda3
akaDova <miraak.dova@gmail.com>
2018-12-17 23:45:43 +0300

    add DependenciesConfiguration class

---
+++ DIContainer/DependenciesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    public class DependenciesConfiguration
+    {
+        public void Register<TDependency, TImplementation>() where TDependency : class
+            where TImplementation : class, new()
+        {
+
+        }
+
+        public void Register(Type dependency, Type impementation)
+        {
+
+        }
+    }
+}
