8870010c20298a77472d6d2d72fa7503c53b2147
akaDova <miraak.dova@gmail.com>
2018-12-17 23:49:42 +0300

    add DependencyProvider class

---
+++ DIContainer/DependencyProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    public class DependencyProvider
+    {
+        public DependencyProvider(DependenciesConfiguration config)
+        {
+
+        }
+
+        public TService Resolve<TService>()
+        {
+            // stub
+            return (TService)Activator.CreateInstance(typeof(TService));
+        }
+    }
+}
