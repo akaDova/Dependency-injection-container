d92548ea46e3153dc887f2925e80af25255bebb4
akaDova <miraak.dova@gmail.com>
2018-12-18 18:11:26 +0300

    test

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -11,11 +11,13 @@ namespace UnitTest
     public class UnitTest
     {
         DependenciesConfiguration config;
+        DependenciesConfiguration config2;
         DependencyProvider provider;
 
         public UnitTest()
         {
             config = new DependenciesConfiguration();
+            config2 = new DependenciesConfiguration();
             
         }
 
@@ -60,16 +62,35 @@ namespace UnitTest
         public void GenericDefinitionTypeResolveTest()
         {
             config.Register(typeof(IGenericImplementation<>), typeof(GenericImplementation1<>));
-            config.Register(typeof(IGenericImplementation<>), typeof(GenericImplementation2<>));
-            provider = new DependencyProvider(config);
-            var instances = provider.Resolve<IGenericImplementation<IImplementation>>();
-            Assert.Equal(2, instances.Count());
 
-            List<Type> expectedInstancesTypes = new List<Type>
-            {
-                typeof(GenericImplementation1<IImplementation>),
-                typeof(GenericImplementation2<IImplementation>)
-            };
+            config2.Register<IGenericImplementation<IImplementation>, GenericImplementation1<IImplementation>>();
+            provider = new DependencyProvider(config);
+            var provider2 = new DependencyProvider(config2);
+            var instance = provider.Resolve<IGenericImplementation<IImplementation>>().First();
+            var instance2 = provider2.Resolve<IGenericImplementation<IImplementation>>().First();
+
+            Assert.Equal(instance.GetType(), instance2.GetType());
+
+            
+            //CollectionAssert.AreEquivalent(expectedInstancesTypes,
+            //    instances.Select((instance) => instance.GetType()).ToList());
+        }
+
+        [Fact]
+        public void ManyRegisters()
+        {
+            config.Register<IImplementation, Implementation>();
+            config.Register<IImplementation, Implemntation3>();
+
+            
+            provider = new DependencyProvider(config);
+            
+            var instance = provider.Resolve<IImplementation>();
+            
+
+            Assert.Equal(2, instance.Count());
+
+
             //CollectionAssert.AreEquivalent(expectedInstancesTypes,
             //    instances.Select((instance) => instance.GetType()).ToList());
         }
@@ -106,6 +127,11 @@ namespace UnitTest
     }
 
     public interface IImplementation2
+    {
+
+    }
+
+    public class Implemntation3: IImplementation
     {
 
     }
