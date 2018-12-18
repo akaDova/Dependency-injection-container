cfeddb67a31948b8abb1b7b14e55060552b77644
akaDova <miraak.dova@gmail.com>
2018-12-18 05:32:09 +0300

    basic implementation

--- DIContainer/DependenciesConfiguration.cs
+++ DIContainer/DependenciesConfiguration.cs
@@ -9,9 +9,14 @@ namespace DIContainer
     public class DependenciesConfiguration
     {
 
-        Dictionary<Type, IEnumerable<Type>> dependencies = new Dictionary<Type, IEnumerable<Type>>(); 
+        Dictionary<Type, IEnumerable<(Type, bool)>> dependencies = new Dictionary<Type, IEnumerable<(Type, bool)>>();
 
-        public void Register<TDependency, TImplementation>() where TDependency : class
+        internal Dictionary<Type, IEnumerable<(Type, bool)>> Dependencies
+        {
+            get;
+        }
+
+        public void Register<TDependency, TImplementation>(bool isSingleton = false) where TDependency : class
             where TImplementation : class, TDependency, new()
         {
             Type dependency = typeof(TDependency);
@@ -28,20 +33,20 @@ namespace DIContainer
             //    dependencies[dependency] = new List<Type>(new Type[] { implementation })
             //        .AsEnumerable();
             //}
-            Register(dependency, implementation);
+            Register(dependency, implementation, isSingleton);
         }
 
-        public void Register(Type dependency, Type implementation)
+        public void Register(Type dependency, Type implementation, bool isSingleton = false)
         {
             if (dependencies.ContainsKey(dependency))
             {
-                List<Type> implementations = dependencies[dependency].ToList();
-                implementations.Add(implementation);
+                List<(Type, bool)> implementations = dependencies[dependency].ToList();
+                implementations.Add((implementation, isSingleton));
                 dependencies[dependency] = implementations.AsEnumerable();
             }
             else
             {
-                dependencies[dependency] = new List<Type>(new Type[] { implementation })
+                dependencies[dependency] = new List<(Type, bool)>(new (Type, bool)[] { (implementation, isSingleton) })
                     .AsEnumerable();
             }
         }
--- DIContainer/DependencyProvider.cs
+++ DIContainer/DependencyProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,17 +9,105 @@ namespace DIContainer
 {
     public class DependencyProvider
     {
+
+        class ImplementationContainer
+        {
+            static object instance;
+            static object syncRoot = new object();
+
+            public static Func<object> GetInstance(Func<object> creator)
+            {
+                object instance = null;
+                return () =>
+                {
+                    if (instance == null)
+                    {
+                        lock (syncRoot)
+                        {
+                            if (instance == null)
+                                instance = creator();
+                        }
+                    }
+                    return instance;
+                };
+                
+                
+            }
+
+             
+        }
+
+        Dictionary<Type, IEnumerable<Func<object>>> dependencies/* = new Dictionary<Type, IEnumerable<(Type, bool)>>()*/;
+        
+        //Dictionary<>
+
         public DependencyProvider(DependenciesConfiguration config)
         {
-
+            dependencies = new Dictionary<Type, IEnumerable<Func<object>>>();
+            foreach(var dependency in config.Dependencies)
+            {
+                dependencies.Add(dependency.Key, ConvertToFactories(dependency.Value));
+            }
         }
 
-        public TService Resolve<TService>()
+        private IEnumerable<Func<object>> ConvertToFactories(IEnumerable<(Type, bool)> tuples)
+        {
+            return from tuple in tuples
+                   select ConvertToFactory(tuple);
+        }
+
+        private Func<object> ConvertToFactory((Type, bool) tuple)
+        {
+            Type type;
+            bool isSingleton;
+            (type, isSingleton) = tuple;
+
+            if (isSingleton)
+            {
+                return ImplementationContainer.GetInstance(() => CreateFromCtor(type));
+            }
+            else
+            {
+                return () => CreateFromCtor(type);
+            }
+        }
+
+        public IEnumerable<TService> Resolve<TService>()
         {
             // stub
-            return (TService)Activator.CreateInstance(typeof(TService));
+            return Resolve(typeof(TService)).Cast<TService>();
         }
 
-        
+        private IEnumerable<object> Resolve(Type type)
+        {
+            return from dependency in dependencies[type]
+                   select dependency();
+        }
+
+        object CreateFromCtor(Type type)
+        {
+            ConstructorInfo constructor = (
+                from ctor in type.GetConstructors()
+                orderby ctor.GetParameters().Length
+                select ctor
+            ).First();
+
+            var parameters = new List<object>();
+            foreach (var currParameter in constructor.GetParameters())
+            {
+                // add some suff
+                parameters.Add(Resolve(currParameter.ParameterType).FirstOrDefault());
+            }
+
+            return constructor.Invoke(parameters.ToArray());
+            
+        }
+
+        private static TService CreateDependency<TService>() where TService : new()
+        {
+            // stub
+            return new TService();
+        }
+
     }
 }
